c6ff5721f8b767395807809c8d5ff8ef85d1f3d0
JialuoChen-Box <140304881+JialuoChen-Box@users.noreply.github.com>
2023-07-23 16:42:03 +0800

    开启 Cloudflare 获取真实IP解决办法 cf.php
    开启 Cloudflare 获取真实IP解决办法

---
+++ cf.php
@@ -0,0 +1,14 @@
+ // Function to get visitor's real IP address (considering Cloudflare)
+        function getRealIP() {
+            if (isset($_SERVER['HTTP_CF_CONNECTING_IP'])) {
+                // Cloudflare
+                return $_SERVER['HTTP_CF_CONNECTING_IP'];
+            } elseif (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+                // Proxy (may have multiple IP addresses in the list)
+                $ip_array = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+                return trim($ip_array[0]);
+            } else {
+                // Direct request (no proxy)
+                return $_SERVER['REMOTE_ADDR'];
+            }
+        }
